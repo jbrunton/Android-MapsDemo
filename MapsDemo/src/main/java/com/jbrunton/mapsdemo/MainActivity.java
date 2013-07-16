eaf0b11a2699b74c99f2377169f86268ae61f31b
jbrunton <john_brunton@hotmail.co.uk>
2013-07-16 21:31:04 +0100

    added my location button to map

--- MapsDemo/src/main/java/com/jbrunton/mapsdemo/MainActivity.java
+++ MapsDemo/src/main/java/com/jbrunton/mapsdemo/MainActivity.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GoogleMap map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        map.setMyLocationEnabled(true);
     }
 }
 
