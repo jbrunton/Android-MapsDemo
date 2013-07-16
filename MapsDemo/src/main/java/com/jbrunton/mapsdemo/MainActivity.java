b53d66c8c70ce69e8010872b9d2818b5707be3f6
jbrunton <john_brunton@hotmail.co.uk>
2013-07-16 22:06:52 +0100

    added search action layout

--- MapsDemo/src/main/java/com/jbrunton/mapsdemo/MainActivity.java
+++ MapsDemo/src/main/java/com/jbrunton/mapsdemo/MainActivity.java
@@ -1,8 +1,12 @@
 package com.jbrunton.mapsdemo;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.SearchView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -15,6 +19,24 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         GoogleMap map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         map.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu from XML
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        // Assumes current activity is the searchable activity
+        if (searchView != null) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+            searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+        }
+
+        return true;
     }
 }
 
